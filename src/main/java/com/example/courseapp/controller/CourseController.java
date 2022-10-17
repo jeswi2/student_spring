582b57417079c5727255e41fd21a51e092796ac4
jeswi2 <jeswimathewm@gmail.com>
2022-10-17 12:59:41 +0530

    ap

---
+++ src/main/java/com/example/courseapp/controller/CourseController.java
@@ -0,0 +1,35 @@
+package com.example.courseapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @GetMapping("/")
+        public String Homepage(){
+        return "Welcome to Home page";
+    }
+
+    @PostMapping("/read")
+    public String Readpage(){
+        return "read page";
+    }
+
+    @GetMapping("/viewall")
+    public List viewall(){
+        List<String> mylist=new ArrayList<>();
+        mylist.add("{'name':'jeswin','age':'21'}");
+        return mylist;
+    }
+
+@GetMapping("/student")
+    public String student(){
+        return "Jeswin";
+}
+
+}
